e7a57003c82736dbe10f4221c1177cc6edd2b83a
Mohammed Aflah <aflumogral7@gmail.com>
2023-10-21 16:28:52 +0530

    Add Path for Firebase Service account

--- server/config/firebase_config.js
+++ server/config/firebase_config.js
@@ -1,7 +1,7 @@
 const admin = require('firebase-admin');
 
 function initializeFirebase(){
-    const serviceAccount = require('../food-express07-firebase-adminsdk-mzq1u-8309f33171.json'); //Private key file path of Firebase project
+    const serviceAccount = require('/etc/secrets/firebase_service_acc_file'); //Private key file path of Firebase project
 
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
